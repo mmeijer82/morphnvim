2d579501ce74d26baffbc0dd10f86c699a4d4a3c
mmeijer82 <58736744+mmeijer82@users.noreply.github.com>
2024-01-19 09:37:42 +0100

    added keymaps and options and float screen for neotree

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,4 @@
 vim.keymap.set('n', '<leader>h', '<C-w>h')
 vim.keymap.set('n', '<leader>l', '<C-w>l')
+vim.keymap.set('n', '<leader>s', ':w<CR>')
 
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2") 
 vim.o.relativenumber = true
+vim.o.shell = "pwsh.exe"
 vim.g.mapleader  = " "
 
 
